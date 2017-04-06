d4e3a6393dfe61e5dbdebfaf53325540f06e329b
cihanb <johndoe@example.com>
2017-04-05 21:51:23 -0700

    updated documentation

---
+++ image_upload.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#step1
+docker pull ---/---
+#step2 - pull the image id
+docker images list
+#step3 - tag the image
+docker tag ---- redislabs/redis
+#step3 - push the image
+docker push redislabs/redis:latest
+
+
