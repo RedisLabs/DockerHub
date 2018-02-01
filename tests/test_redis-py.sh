43653d6ea73240c3f8feac43a8d5d5767aab05e5
cihanb <johndoe@example.com>
2018-01-31 19:20:45 -0800

    removed additional -t from docker exec

--- tests/test_redis-py.sh
+++ tests/test_redis-py.sh
@@ -45,7 +45,7 @@ test_oss_db(){
     echo "docker run -d --name redis-python redislabs/redis-py"
     docker run -d --name redis-python redislabs/redis-py
 
-    echo "docker exec -it redis-python \"python\" /usr/src/app/test_db.py $oss_host_name $oss_db_port"
+    echo "docker exec -i redis-python \"python\" /usr/src/app/test_db.py $oss_host_name $oss_db_port"
     docker exec -i redis-python "python" /usr/src/app/test_db.py $oss_host_name $oss_db_port
 }
 
@@ -79,11 +79,11 @@ test_enterprise_db(){
     curl -k -u "cihan@redislabs.com:redislabs123" --request POST --url "https://localhost:9443/v1/bdbs" --header 'content-type: application/json' --data '{"name":"db1","type":"redis","memory_size":102400,"port":'$ent_db_port'}'
 
     #get the container ip
-    echo "docker exec -it rp ifconfig | grep 172. | cut -d\":\" -f 2 | cut -d\" \" -f 1" 
-    cmd="docker exec -it rp ifconfig | grep 172. | cut -d\":\" -f 2 | cut -d\" \" -f 1"
+    echo "docker exec -i rp ifconfig | grep 172. | cut -d\":\" -f 2 | cut -d\" \" -f 1" 
+    cmd="docker exec -i rp ifconfig | grep 172. | cut -d\":\" -f 2 | cut -d\" \" -f 1"
     ent_host_name=$(eval $cmd)
 
-    echo "docker exec -it redis-python \"python\" /usr/src/app/test_db.py $ent_host_name $ent_db_port"
+    echo "docker exec -i redis-python \"python\" /usr/src/app/test_db.py $ent_host_name $ent_db_port"
     docker exec -i redis-python "python" /usr/src/app/test_db.py $ent_host_name $ent_db_port
 }
 
